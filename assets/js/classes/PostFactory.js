e3f96f206656dbd5823991dfa94771dc9b217708
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 01:10:19 -0500

    ✨ Add postFactory

---
+++ assets/js/classes/PostFactory.js
@@ -0,0 +1,23 @@
+import {fetchAllAuthors,fetchAllTags} from '../utils/dataFetcher.js'
+
+
+const createPost=({id,title,subTitle,image,body,createDate,likes,author,tags},AllTags,authors)=>{
+    return {
+        id,
+        title,
+        subTitle,
+        image,
+        body,
+        createDate,
+        likes,
+        author:authors.find(apiAuthor=>apiAuthor.id==author),
+        tags:AllTags.filter(tag=>tags.includes(tag.id))
+    }
+}
+
+
+export default async(posts)=>{
+    const tags=await fetchAllTags();
+    const authors=await fetchAllAuthors();
+    return posts.map(post=>createPost(post,tags,authors));
+}
