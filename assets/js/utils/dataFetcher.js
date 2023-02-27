a971b5e5fa5bd63824954e04320b7022e72451d0
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 15:11:35 -0500

    ✨ Update Datafetcher and add PUT method

--- assets/js/utils/dataFetcher.js
+++ assets/js/utils/dataFetcher.js
@@ -1,23 +1,24 @@
 import { API_URL } from '../constants/constants.js';
 
-const fetchAllPosts =async () => {
-    return  await (await fetch(`${API_URL}/posts`)).json();
-    };
-const fetchPostById =async (id) => {
-    return  await (await fetch(`${API_URL}/posts/${id}`)).json();
-    };
-const fetchAllTags =async () => {
-    return  await (await fetch(`${API_URL}/tags`)).json();
-    };
-const fetchAllAuthors =async () => {
-    return  await (await fetch(`${API_URL}/authors`)).json();
-    };
-const fetchAllUsers =async () => {
-    return  await (await fetch(`${API_URL}/users`)).json();
-    };
-const fetchCommentsByPostId =async (id) => {
-    return  await (await fetch(`${API_URL}/comments?postId=${id}`)).json();
-    };
+const fetchAllPosts =async () => (await fetch(`${API_URL}/posts?_sort=createDate,id&_order=desc`)).json();
+const fetchPostById =async (id) => (await fetch(`${API_URL}/posts/${id}`)).json();
+const fetchAllTags =async () => (await fetch(`${API_URL}/tags`)).json();
+const fetchAllAuthors =async () => (await fetch(`${API_URL}/authors`)).json();
+const fetchAllUsers =async () => (await fetch(`${API_URL}/users`)).json();
+const fetchCommentsByPostId =async (id) => (await fetch(`${API_URL}/comments?postId=${id}`)).json();
+const addlikeToPost =async (postId,newLikesNumber,post) => {
+    await fetch(`${API_URL}/posts/${postId}`,{
+    method: 'PUT',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({...post,likes:newLikesNumber,
+        author:post.author.id,
+        tags:post.tags.map(tag=>tag.id) 
+    })
+})
+return {...post,likes:newLikesNumber}
+};
+const deletePost =async (id)=>{await fetch(`${API_URL}/posts/${id}`,{method: 'DELETE'})
+}
 
 export {
     fetchAllPosts,
@@ -25,6 +26,8 @@ export {
     fetchAllTags,
     fetchAllAuthors,
     fetchAllUsers,
-    fetchCommentsByPostId
+    fetchCommentsByPostId,
+    addlikeToPost,
+    deletePost
 }
 
