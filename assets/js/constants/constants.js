7ecfa7c5a9ebf570df6cb1a1cc09b730375398e8
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 01:07:55 -0500

    🔧 Add constants file

---
+++ assets/js/constants/constants.js
@@ -0,0 +1,7 @@
+// -------------- API CONSTANTS ----------------
+export const URL = "http://localhost:5500";
+export const API_URL = "https://plaid-checkered-roquefort.glitch.me";
+
+// -------------- Debounce timer ----------------
+export const debounceTimer = 1000;
+
