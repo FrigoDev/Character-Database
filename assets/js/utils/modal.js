cfd5688e1cfc3cda80fd7cadc520153feac59287
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 19:26:03 -0500

    ✨ Add modal display and close functionality

---
+++ assets/js/utils/modal.js
@@ -0,0 +1,17 @@
+const  btn = document.getElementById("open-modal");
+const modal = document.getElementById("modal");
+const close = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+close.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
