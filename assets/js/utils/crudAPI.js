9bfc436363c251058a5969e3974d3850b1a00e63
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 22:43:18 -0500

    ✨ 🎨 Add few features and format to crudAPI

--- assets/js/utils/crudAPI.js
+++ assets/js/utils/crudAPI.js
@@ -1,38 +1,53 @@
-import { API_URL } from '../constants/constants.js';
-const fetchAllPosts =async () => (await fetch(`${API_URL}/posts?_sort=createDate,id&_order=desc`)).json();
-const fetchPostById =async (id) => (await fetch(`${API_URL}/posts/${id}`)).json();
-const fetchAllTags =async () => (await fetch(`${API_URL}/tags`)).json();
-const fetchAllAuthors =async () => (await fetch(`${API_URL}/authors`)).json();
-const fetchAllUsers =async () => (await fetch(`${API_URL}/users`)).json();
-const fetchCommentsByPostId =async (id) => (await fetch(`${API_URL}/comments?postId=${id}`)).json();
-const addlikeToPost =async (postId,newLikesNumber,post) => {
-    await fetch(`${API_URL}/posts/${postId}`,{
-    method: 'PUT',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({...post,likes:newLikesNumber,
-        author:post.author.id,
-        tags:post.tags.map(tag=>tag.id) 
-    })
- })
- return {...post,likes:newLikesNumber}
+import { API_URL } from "../constants/constants.js";
+const fetchAllPosts = async () =>
+  (await fetch(`${API_URL}/posts?_sort=createDate,id&_order=desc`)).json();
+const fetchPostById = async (id) =>
+  (await fetch(`${API_URL}/posts/${id}`)).json();
+const fetchAllTags = async () => (await fetch(`${API_URL}/tags`)).json();
+const fetchAllAuthors = async () => (await fetch(`${API_URL}/authors`)).json();
+const fetchAllUsers = async () => (await fetch(`${API_URL}/users`)).json();
+const fetchCommentsByPostId = async (id) =>
+  (await fetch(`${API_URL}/comments?postId=${id}`)).json();
+const addlikeToPost = async (postId, newLikesNumber, post) => {
+  await fetch(`${API_URL}/posts/${postId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ...post,
+      likes: newLikesNumber,
+      author: post.author.id,
+      tags: post.tags.map((tag) => tag.id),
+    }),
+  });
+  return { ...post, likes: newLikesNumber };
 };
-const deletePost =async (id)=>{await fetch(`${API_URL}/posts/${id}`,{method: 'DELETE'})
-}
-const createComment =async (comment)=>{await fetch(`${API_URL}/comments`,{
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(comment)
-    })};
+const deletePost = async (id) => {
+  await fetch(`${API_URL}/posts/${id}`, { method: "DELETE" });
+};
+const createComment = async (comment) => {
+  await fetch(`${API_URL}/comments`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(comment),
+  });
+};
 
+const createPost = async (post) => {
+  await fetch(`${API_URL}/posts`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(post),
+  });
+};
 export {
-    fetchAllPosts,
-    fetchPostById,
-    fetchAllTags,
-    fetchAllAuthors,
-    fetchAllUsers,
-    fetchCommentsByPostId,
-    addlikeToPost,
-    deletePost,
-    createComment
-}
-
+  fetchAllPosts,
+  fetchPostById,
+  fetchAllTags,
+  fetchAllAuthors,
+  fetchAllUsers,
+  fetchCommentsByPostId,
+  addlikeToPost,
+  deletePost,
+  createComment,
+  createPost,
+};
