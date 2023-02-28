31e0f38321bdfcf5bb9271c78bfdff52a5bfaf2b
FrigoAlex <alejo012903@hotmail.com>
2023-02-27 19:27:41 -0500

    ✨ Add comments functionality

---
+++ assets/js/components/comment.js
@@ -0,0 +1,10 @@
+export default (comment)=>`<div class="comment" bis_skin_checked="1">
+<div class="comment-author" bis_skin_checked="1">
+  ${`${comment.user.name} ${comment.user.lastName}`}
+</div>
+<div class="post-comment-content" bis_skin_checked="1">
+  ${
+    comment.comment
+  }
+</div>
+</div>`
