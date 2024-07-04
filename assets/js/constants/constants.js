080a56c076b7b8ea6c9cc1cc0d9f61db41ef4af2
FrigoAlex <alejo012903@hotmail.com>
2024-07-04 14:06:15 -0500

    🐛 🚀Update URL constant to host

--- assets/js/constants/constants.js
+++ assets/js/constants/constants.js
@@ -1,5 +1,5 @@
 // -------------- API CONSTANTS ----------------
-export const URL = "http://localhost:5500";
+export const URL = "https://characterdatabase.vercel.app/";
 export const API_URL = "https://plaid-checkered-roquefort.glitch.me";
 
 // -------------- Debounce timer ----------------
